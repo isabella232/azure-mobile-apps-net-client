613a8cf084d8e18cdc68456b88b64548c37cae07
Matthew Leibowitz <mattleibow@live.com>
2020-11-26 03:17:11 +0200

    Migrate to using xharness for device unit tests

--- e2etests/DeviceTests.Android/MainActivity.cs
+++ e2etests/DeviceTests.Android/MainActivity.cs
@@ -5,12 +5,8 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using DeviceTests.Shared.Tests;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Threading.Tasks;
-using UnitTests.HeadlessRunner;
 using Xunit.Runners.UI;
 
 namespace DeviceTests.Android
@@ -26,24 +22,6 @@ namespace DeviceTests.Android
         protected override void OnCreate(Bundle bundle)
         {
             Xamarin.Essentials.Platform.Init(this, bundle);
-
-            var hostIp = Intent.Extras?.GetString("HOST_IP", null);
-            var hostPort = Intent.Extras?.GetInt("HOST_PORT", 63559) ?? 63559;
-
-            if (!string.IsNullOrEmpty(hostIp))
-            {
-                // Run the headless test runner for CI
-                Task.Run(() =>
-                {
-                    return UnitTests.HeadlessRunner.Tests.RunAsync(new TestOptions
-                    {
-                        Assemblies = new List<Assembly> { typeof(Shared_Tests).Assembly },
-                        NetworkLogHost = hostIp,
-                        NetworkLogPort = hostPort,
-                        Format = TestResultsFormat.XunitV2,
-                    });
-                });
-            }
 
             // tests can be inside the main assembly
             AddTestAssembly(Assembly.GetExecutingAssembly());
---
+++ e2etests/DeviceTests.Android/TestInstrumentation.cs
@@ -0,0 +1,122 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using DeviceTests.Shared.TestPlatform;
+using Microsoft.DotNet.XHarness.TestRunners.Common;
+using Microsoft.DotNet.XHarness.TestRunners.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Xamarin.Essentials;
+
+namespace DeviceTests.Android
+{
+    [Instrumentation(Name = "com.microsoft.azure.devicetests.TestInstrumentation")]
+    public class TestInstrumentation : Instrumentation
+    {
+        private string resultsFileName;
+
+        protected TestInstrumentation()
+        {
+        }
+
+        protected TestInstrumentation(IntPtr handle, JniHandleOwnership transfer)
+            : base(handle, transfer)
+        {
+        }
+
+        public override void OnCreate(Bundle arguments)
+        {
+            base.OnCreate(arguments);
+
+            resultsFileName = arguments.GetString("results-file-name", "TestResults.xml");
+
+            Start();
+        }
+
+        public override async void OnStart()
+        {
+            base.OnStart();
+
+            var bundle = new Bundle();
+
+            var runner = new TestRunner(resultsFileName);
+            runner.TestsCompleted += (sender, results) =>
+            {
+                bundle.PutString("test-execution-summary",
+                    $"Tests run: {results.ExecutedTests} " +
+                    $"Passed: {results.PassedTests} " +
+                    $"Inconclusive: {results.InconclusiveTests} " +
+                    $"Failed: {results.FailedTests} " +
+                    $"Ignored: {results.SkippedTests}");
+
+                bundle.PutLong("return-code", results.FailedTests == 0 ? 0 : 1);
+            };
+
+            await runner.RunAsync();
+
+            if (File.Exists(runner.TestsResultsFinalPath))
+                bundle.PutString("test-results-path", runner.TestsResultsFinalPath);
+
+            if (bundle.GetLong("return-code", -1) == -1)
+                bundle.PutLong("return-code", 1);
+
+            Finish(Result.Ok, bundle);
+        }
+
+        private class TestRunner : AndroidApplicationEntryPoint
+        {
+            private readonly string resultsPath;
+
+            public TestRunner(string resultsFileName)
+            {
+                var docsType = global::Android.OS.Environment.DirectoryDocuments;
+                var docsDir = Application.Context.GetExternalFilesDir(docsType).AbsolutePath;
+                if (!Directory.Exists(docsDir))
+                    Directory.CreateDirectory(docsDir);
+
+                resultsPath = Path.Combine(docsDir, resultsFileName);
+            }
+
+            public override TextWriter Logger => null;
+
+            public override string TestsResultsFinalPath => resultsPath;
+
+            protected override int? MaxParallelThreads => System.Environment.ProcessorCount;
+
+            protected override IDevice Device { get; } = new TestDevice();
+
+            protected override IEnumerable<TestAssemblyInfo> GetTestAssemblies()
+            {
+                yield return new TestAssemblyInfo(typeof(MainActivity).Assembly, typeof(MainActivity).Assembly.Location);
+                yield return new TestAssemblyInfo(typeof(E2ETestBase).Assembly, typeof(E2ETestBase).Assembly.Location);
+            }
+
+            protected override void TerminateWithSuccess()
+            {
+            }
+        }
+
+        private class TestDevice : IDevice
+        {
+            public string BundleIdentifier => AppInfo.PackageName;
+
+            public string UniqueIdentifier => Guid.NewGuid().ToString("N");
+
+            public string Name => DeviceInfo.Name;
+
+            public string Model => DeviceInfo.Model;
+
+            public string SystemName => DeviceInfo.Platform.ToString();
+
+            public string SystemVersion => DeviceInfo.VersionString;
+
+            public string Locale => CultureInfo.CurrentCulture.Name;
+        }
+    }
+}
